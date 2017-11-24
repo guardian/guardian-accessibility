a017237a3fe120108a726ff141441c178db56820
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-11-24 10:31:12 +0000

    Update project to build  riff-raff deployable assets

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+virtualenv ve
+ve/bin/pip install mkdocs
+ve/bin/mkdocs build --clean
 
-pip install mkdocs
-mkdocs build --clean
+NODE_VERSION="6.2.1"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm use ${NODE_VERSION}
+
+npm install
+npm run riffraff-artefact
