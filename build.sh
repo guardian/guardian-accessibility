a650dd46834df56f6bc74402b2f8231276ca4181
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-11-24 10:55:34 +0000

    Update node version

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ virtualenv ve
 ve/bin/pip install mkdocs
 ve/bin/mkdocs build --clean
 
-NODE_VERSION="6.2.1"
+NODE_VERSION="8.5"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
