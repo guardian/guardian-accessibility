8070583ef8dda063e35f8a1ab3c904ce85a07e40
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-11-27 12:02:51 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -11,3 +11,5 @@ nvm use ${NODE_VERSION}
 
 npm install
 npm run riffraff-artefact
+
+aws s3 cp --acl bucket-owner-full-control --region=eu-west-1 --recursive site s3://$1/tools::accessibility.gutools/$2/site
